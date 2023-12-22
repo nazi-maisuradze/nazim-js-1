dbb7bb9d942b075e58b9eb97645f3a841f03d7ce
nazi-maisuradze <146000732+nazi-maisuradze@users.noreply.github.com>
2023-12-23 01:17:32 +0400

    task

---
+++ script.js
@@ -0,0 +1,43 @@
+// task 1 
+let userAge = 100;
+// if (userAge <50) {
+//     console.log("ნაკლებია");
+// }
+// else if (userAge>20) {
+//     console.log("მეტია");
+// }
+// else {
+//     console.log ("error");
+// }
+
+let result = userAge <50 ?"naklebia": userAge>20 ? "metia" : "error";
+console.log(result);
+
+
+//task 2 
+let userName = "მარიამ"
+if (userName == "მარიამ") {
+    console.log (true);
+}
+else {
+    console.log (false);
+}
+
+// task 3 
+let userName1 = "მარიამ"
+switch ("მარიამ") {
+    case "მარიამ" :
+        console.log (true);
+        break;
+    default : 
+    console.log ("error")
+}
+
+//task 4
+let num =17;
+if (num % 2 ==0) {
+    console.log ("ლუწი");
+}
+else {console.log ("კენტი"); 
+}
+
